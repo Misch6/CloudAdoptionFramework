aca282ec2fca525ab3901241deca5d4ba286c1dd
Misch <121165921+Misch6@users.noreply.github.com>
2023-07-17 20:53:19 +0200

    Update ResourceType.cs

--- ready/AzNamingTool/Models/ResourceType.cs
+++ ready/AzNamingTool/Models/ResourceType.cs
@@ -17,7 +17,7 @@ namespace AzureNamingTool.Models
         {
             get { return _ShortName; }   // get method
             //set => _ShortName = value?.ToLower();   // set method
-            set  => _ShortName;
+            set  => _ShortName = ShortName;
         }
         public string Scope { get; set; }
         public string LengthMin { get; set; }
